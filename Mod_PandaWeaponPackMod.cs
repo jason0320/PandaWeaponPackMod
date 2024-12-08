c12f181baa876a04fba6cb7a036be98d111de853
jason0320 <jasonnosaj0320@gmail.com>
2024-12-08 17:39:32 +0800

    加入專案檔案。

---
+++ Mod_PandaWeaponPackMod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace PandaWeaponPackMod
+{
+    [BepInPlugin("panda.weaponpack.mod", "Panda's Weapon Pack Mod", "1.0.0.0")]
+    public class Mod_PandaWeaponPackMod : BaseUnityPlugin
+    {
+        private void OnStartCore()
+        {
+            string pathToExcelFile = Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location) + "/WeaponPack.xlsx";
+            SourceManager sources = Core.Instance.sources;
+            ModUtil.ImportExcel(pathToExcelFile, "Thing", sources.things);
+        }
+    }
+}
